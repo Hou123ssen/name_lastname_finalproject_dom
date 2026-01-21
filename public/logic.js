2b1674c622a1dba8b978674d98df7ee45676643a
Hou123ssen <houssend6@gmail.com>
2026-01-21 23:41:48 +0100

    add responsive

--- public/logic.js
+++ public/logic.js
@@ -1,4 +1,3 @@
-
 // NENU :
 let filterBtns = document.querySelectorAll(".choose");
 let foodItems = document.querySelectorAll(".food-item");
@@ -33,15 +32,13 @@ filterBtns.forEach((btn) => {
   });
 });
 
-
-// TESTMONIALS : 
+// TESTMONIALS :
 
 let slides = document.querySelectorAll(".slide");
 let buttons = document.querySelectorAll(".tab-btn");
 
 let currentIndex = 0;
 let intervalId;
-
 
 function startCarousel() {
   intervalId = setInterval(() => {
@@ -57,22 +54,21 @@ function startCarousel() {
 
 startCarousel();
 
-
 buttons.forEach((btn) => {
   btn.addEventListener("click", () => {
-    clearInterval(intervalId); 
+    clearInterval(intervalId);
 
     slides[currentIndex].classList.remove("active");
     buttons[currentIndex].classList.remove("active");
 
-    currentIndex = Number(btn.dataset.index); 
+    currentIndex = Number(btn.dataset.index);
 
     slides[currentIndex].classList.add("active");
     buttons[currentIndex].classList.add("active");
   });
 });
 
-// EVENT : 
+// EVENT :
 
 const Slider = document.querySelector(".content-events");
 const Slides = document.querySelectorAll(".content-events > div");
@@ -80,7 +76,6 @@ const Slides = document.querySelectorAll(".content-events > div");
 let index = 0;
 const slideWidth = slides[0].offsetWidth;
 const total = slides.length;
-
 
 const firstClone = Slides[0].cloneNode(true);
 Slider.appendChild(firstClone);
@@ -90,7 +85,6 @@ function moveSlider() {
   Slider.style.transition = "transform 0.8s ease";
   Slider.style.transform = `translateX(-${index * slideWidth}px)`;
 
- 
   if (index === total) {
     setTimeout(() => {
       Slider.style.transition = "none";
@@ -101,3 +95,29 @@ function moveSlider() {
 }
 
 setInterval(moveSlider, 2500);
+
+// gallery :
+
+let sldes = document.querySelectorAll(".all-gallery > div");
+let activeIndex = 2;
+
+function updateCarousel() {
+  sldes.forEach((slid, index) => {
+    let offset = index - activeIndex;
+
+    if (offset > sldes.length / 2) offset -= sldes.length;
+    if (offset < -sldes.length / 2) offset += sldes.length;
+
+    slid.style.transform = `translateX(${offset * 300}px) scale(${offset === 0 ? 1.2 : 0.9})`;
+
+    slid.style.zIndex = offset === 0 ? 3 : 1;
+    slid.style.opacity = Math.abs(offset) > 2 ? 0 : 1;
+  });
+}
+
+updateCarousel();
+
+setInterval(() => {
+  activeIndex = (activeIndex + 1) % sldes.length;
+  updateCarousel();
+}, 2500);
